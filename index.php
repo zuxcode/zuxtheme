7189d3b06c4297db0109ce0174454bb1a338366e
zuxcode <codeauthor1@gmail.com>
2024-10-12 03:46:56 +0100

    Feat: hero section in progress

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <div class="container mx-auto my-8">
 
 	<?php if ( have_posts() ) : ?>
-		<?php
+		<!-- <?php
 		while ( have_posts() ) :
 			the_post();
-			?>
+			?> -->
 
 			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,46 @@
-const theme = require('./theme.json');
+const theme = require("./theme.json");
 const tailpress = require("@jeffreyvr/tailwindcss-tailpress");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './*.php',
-        './**/*.php',
-        './resources/css/*.css',
-        './resources/js/*.js',
-        './safelist.txt'
-    ],
-    theme: {
-        container: {
-            padding: {
-                DEFAULT: '1rem',
-                sm: '2rem',
-                lg: '0rem'
-            },
-        },
-        extend: {
-            colors: tailpress.colorMapper(tailpress.theme('settings.color.palette', theme)),
-            fontSize: tailpress.fontSizeMapper(tailpress.theme('settings.typography.fontSizes', theme))
-        },
-        screens: {
-            'xs': '480px',
-            'sm': '600px',
-            'md': '782px',
-            'lg': tailpress.theme('settings.layout.contentSize', theme),
-            'xl': tailpress.theme('settings.layout.wideSize', theme),
-            '2xl': '1440px'
-        }
+  content: [
+    "./*.php",
+    "./**/*.php",
+    "./resources/css/*.css",
+    "./resources/js/*.js",
+    "./safelist.txt",
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "0rem",
+      },
     },
-    plugins: [
-        tailpress.tailwind
-    ]
+    extend: {
+      colors: tailpress.colorMapper(
+        tailpress.theme("settings.color.palette", theme)
+      ),
+      fontSize: tailpress.fontSizeMapper(
+        tailpress.theme("settings.typography.fontSizes", theme)
+      ),
+      backgroundImage: {
+        hero: "radial-gradient(at 41% 20%, hsla(28,100%,74%,1) 0px, transparent 50%),radial-gradient(at 80% 0%, hsla(189,100%,56%,1) 0px, transparent 50%) radial-gradient(at 0% 50%, hsla(355,100%,93%,1) 0px, transparent 50%), radial-gradient(at 80% 50%, hsla(340,100%,76%,1) 0px, transparent 50%),radial-gradient(at 0% 100%, hsla(22,100%,77%,1) 0px, transparent 50%), radial-gradient(at 80% 100%, hsla(242,100%,70%,1) 0px, transparent 50%),radial-gradient(at 0% 0%, hsla(343,100%,76%,1) 0px, transparent 50%)",
+      },
+      backgroundColor: {
+        red: "hsla(0,100%,50%,1)",
+      },
+      
+    },
+    screens: {
+      xs: "480px",
+      sm: "600px",
+      md: "782px",
+      lg: tailpress.theme("settings.layout.contentSize", theme),
+      xl: tailpress.theme("settings.layout.wideSize", theme),
+      "2xl": "1440px",
+    },
+  },
+  plugins: [tailpress.tailwind],
 };
